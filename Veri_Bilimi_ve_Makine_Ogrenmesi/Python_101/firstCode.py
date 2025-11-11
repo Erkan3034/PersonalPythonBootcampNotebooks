ed256532b40116a4da661851bc4b376ddc30bdfa
Erkan3034 <turguterkan55@gmail.com>
2025-11-11 19:07:52 +0300

    Remove untitled.py and enhance firstCode.py with variable declarations and data type examples

--- Veri_Bilimi_ve_Makine_Ogrenmesi/Python_101/firstCode.py
+++ Veri_Bilimi_ve_Makine_Ogrenmesi/Python_101/firstCode.py
@@ -1,1 +1,20 @@
-print("Hello World")
+#Variables===========
+x=5
+print(x)
+
+#Data Types=========== 
+#String
+name="John"
+print(name)
+
+#Integer
+age=20
+print(age)
+
+#Float
+height=1.75
+print(height)
+
+#Boolean
+is_student=True
+print(is_student)
